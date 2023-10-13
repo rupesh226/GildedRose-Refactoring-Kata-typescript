8e489ed78b92ccd166097f06d711a488167a2bd3
rupesh226 <boyz.226@gmail.com>
2023-10-13 23:25:56 +0800

    Added switch statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -35,106 +35,40 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       // This can be convert into a switch statement
 
-      if (
-        this.items[i].name != AGED_BRIE &&
-        this.items[i].name != BACKSTAGE_PASS
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS) {
-            // Could be default ? because AGED_BRIE BACKSTAGE_PASS and SULFURAS condition are negative ? (decrease quality)
-            this.items[i].decreseQuality();
+      switch (this.items[i].name) {
+        case AGED_BRIE:
+          this.items[i].increaseQuality();
+          if (this.items[i].sellIn < 0) {
+            this.items[i].increaseQuality();
           }
-        }
-      } else {
-        // this is for AGED_BRIE and BACKSTAGE_PASS
-        this.items[i].increaseQuality();
-        if (this.items[i].name == BACKSTAGE_PASS) {
-          // case for BACKSTAGE_PASS
+          break;
+
+        case BACKSTAGE_PASS:
+          this.items[i].increaseQuality();
           if (this.items[i].sellIn < 11) {
             this.items[i].increaseQuality();
           }
           if (this.items[i].sellIn < 6) {
             this.items[i].increaseQuality();
           }
-        }
-      }
-      if (this.items[i].name != SULFURAS) {
-        // decrease sellIn for all except SULFURAS
-        this.items[i].decreaseSellIn();
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE) {
-          if (this.items[i].name != BACKSTAGE_PASS) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
-                this.items[i].decreseQuality();
-              }
-            }
-          } else {
-            //for BACKSTAGE_PASS SULFURAS and default
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality = 0;
           }
-        } else {
-          this.items[i].increaseQuality();
-        }
+          break;
+
+        case SULFURAS:
+          // No update
+          break;
+        default:
+          this.items[i].decreseQuality();
+          if (this.items[i].sellIn < 0) {
+            this.items[i].decreseQuality();
+          }
+          break;
       }
+      this.items[i].decreaseSellIn();
     }
 
     return this.items;
   }
 }
-
-/**
- * overall BACKSTAGE_PASS condistion 
-
-if (this.items[i].sellIn < 0)) {
-  this.items[i].quality =
-  this.items[i].quality - this.items[i].quality;
-  or 
-  item.quality = 0;
-return ;
-}
-
-Line number 50 
-
-item.increaseQuality();
-
-line number 54 
-
-if (this.items[i].sellIn < 11) {
-  this.items[i].increaseQuality();
-}
-if (this.items[i].sellIn < 6) {
-  this.items[i].increaseQuality();
-}
-
-*/
-
-/**
- * Overall AGED_BRIE condition
- *
- * from line number 51 -> his.items[i].increaseQuality();
- *
- * from line # 66 & 80 -> if (this.items[i].sellIn < 0) his.items[i].increaseQuality();
- *
- */
-
-/**
- *
- * SULFURAS
- *
- * No update ?
- *
- */
-
-/**
- * Default condition
- * Line # 45 - this.items[i].decreseQuality();
- * Line # 71 - if (this.items[i].sellIn < 0) {this.items[i].decreseQuality();}
- */
-
-/**
- *
- * after switch statment - decreaseSellIn
- */
